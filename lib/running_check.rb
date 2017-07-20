c1350239f2e79d6a9e6c12dc07152081188778c7
Misty De Meo <mistydemeo@gmail.com>
2017-07-19 19:24:12 -0700

    warn_if_running: print to stderr

--- lib/running_check.rb
+++ lib/running_check.rb
@@ -2,6 +2,6 @@ require_relative "ps"
 
 def warn_if_running
   if GoodDog::PS.command_running? "Stay"
-    puts "WARNING: Stay is currently running. Changes to the database may not be kept!"
+    $stderr.puts "WARNING: Stay is currently running. Changes to the database may not be kept!"
   end
 end
